6cbfb89add9176479dd6f94ab6fdf28b1f80fc06
Maryna Karamanova <karamanova91@gmail.com>
2023-02-25 14:18:14 +0200

    updated folder names

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 import { Section } from "components/section/Section";
-import { FeedbackOptions } from "components/FeedbackOptions/FeedbackOptions";
+import { FeedbackOptions } from "components/feedbackoptions/FeedbackOptions";
 import { Statistics } from "components/statistics/Statistics";
-import Notification from 'components/Notification/Notification';
+import Notification from 'components/notification/Notification';
 import {NotificationMessage} from './App.styled';
 import { Container } from './App.styled';
 export class App extends Component {
